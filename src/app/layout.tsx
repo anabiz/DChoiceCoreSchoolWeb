306f94e0c7961bf19ad4a0e199449bccb3005d7b
Anthony <anthony.iwuji@fcmb.com>
2025-12-26 22:27:17 +0100

    add facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import "../styles/index.css";
 import { Providers } from "./providers";
 import Analytics from "@/components/Common/Analytics";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +25,33 @@ export default function RootLayout({
       <head />
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2135853057153873');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2135853057153873&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Providers>
-          {/* <Header /> */}
           {children}
           <Footer />
           <Analytics />
