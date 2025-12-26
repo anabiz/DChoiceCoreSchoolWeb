e07a12fb73dda15ea44186c874f3896058b070b6
Anthony <anthony.iwuji@fcmb.com>
2025-12-26 17:27:39 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,9 @@ const nextConfig = {
         port: "",
       },
     ],
+  },
+  experimental: {
+    optimizeCss: true,
   },
 };
 
